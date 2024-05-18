cf6c5b3c276424c2c8c913984ff1b1200b42aa8b
Vladimir Volkov <vovolkov@edu.hse.ru>
2024-05-19 01:22:17 +0300

    User profile refactoring and implementation of base_generic.html

--- RUSA-yikilev/Rusa/Constructor/urls.py
+++ RUSA-yikilev/Rusa/Constructor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import map_view, save_coordinates, get_lines, get_routes
 
 urlpatterns = [
-    path('', map_view),
+    path('', map_view, name='home'),
     path('save_coordinates/', save_coordinates, name='save_coordinates'),
     path('get_lines/', get_lines, name='get_lines'),
     path('get_routes/', get_routes, name='get_routes'),
--- RUSA-yikilev/Rusa/Rusa/settings.py
+++ RUSA-yikilev/Rusa/Rusa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "Registration",
     "Constructor",
     "Userprofile",
+    "django_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = "Rusa.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'Rusa/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
