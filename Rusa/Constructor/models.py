e7098e08297a17cc7f4e47cd3f497cc87a348093
metrafun <176291@list.ru>
2024-05-19 19:47:51 +0300

    roud length added

--- Rusa/Constructor/models.py
+++ Rusa/Constructor/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Line(models.Model):
     name = models.TextField(null=True)
     coordinates = models.TextField(null=True)
+    length = models.DecimalField(null=True)
