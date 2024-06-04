1ca964e3f9814e1df64443d911d256a8e304b666
metrafun <176291@list.ru>
2024-06-04 19:29:26 +0300

    route_page design updated, route_list preview added

--- Rusa/Constructor/views.py
+++ Rusa/Constructor/views.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 from .models import Line
 import json
 from django.core import serializers
+import json
+import datetime
 
 def map_view(request):
     api_key = settings.YANDEX_MAPS_API_KEY
@@ -47,27 +49,32 @@ def route_list(request):
         sort_by = '-' + sort_by
 
     routes = Line.objects.all().order_by(sort_by)
+    for route in routes:
+        route.map_url = create_map_url(route)
+
     return render(request, 'route_list.html', {'routes': routes})
 
 def route_page(request, route_id):
     route = get_object_or_404(Line, pk=route_id)
     route_length = route.length / 1000
-    route_time = route_length / 5
-    if route_time < 1:
-        route_time_minutes = round(route_time * 60)
-        if 5 <= route_time_minutes <= 20 or route_time_minutes % 10 == 0 or 5 <= route_time_minutes % 10 <= 9:
-            route_time_str = f"{route_time_minutes:.0f} минут"
-        elif route_time_minutes % 10 == 1:
-            route_time_str = f"{route_time_minutes:.0f} минута"
-        else:
-            route_time_str = f"{route_time_minutes:.0f} минуты"
-    else:
-        if 2 <= route_time < 5:
-            route_time_str = f"{route_time:.2f} часа"
-        elif route_time >= 5:
-            route_time_str = f"{route_time:.2f} часов"
-        else:
-            route_time_str = f"{route_time:.2f} час"
+    route_time_sec = round(route_length / 5 * 3600)
+    route_time = datetime.timedelta(seconds=route_time_sec)
+    route_time_str = str(route_time)
+    # if route_time < 1:
+    #     route_time_minutes = round(route_time * 60)
+    #     if 5 <= route_time_minutes <= 20 or route_time_minutes % 10 == 0 or 5 <= route_time_minutes % 10 <= 9:
+    #         route_time_str = f"{route_time_minutes:.0f} минут"
+    #     elif route_time_minutes % 10 == 1:
+    #         route_time_str = f"{route_time_minutes:.0f} минута"
+    #     else:
+    #         route_time_str = f"{route_time_minutes:.0f} минуты"
+    # else:
+    #     if 2 <= route_time < 5:
+    #         route_time_str = f"{route_time:.2f} часа"
+    #     elif route_time >= 5:
+    #         route_time_str = f"{route_time:.2f} часов"
+    #     else:
+    #         route_time_str = f"{route_time:.2f} час"
     context = {
         'route': route,
         'route_time': route_time_str,
@@ -77,3 +84,43 @@ def route_page(request, route_id):
     }
     return render(request, 'route_page.html', context)
 
+def create_map_url(route):
+    # Первая и последняя координаты для меток
+    coordinates = json.loads(route.coordinates)
+    start = coordinates[0]
+    end = coordinates[-1]
+
+    # Параметры меток
+    start_marker = f"{start[0]},{start[1]},pm2ntl3"  # Красная метка в начале
+    # end_marker = f"{end[0]},{end[1]}," + "{% static 'img/finish_flag.png' %}"  # Пользовательское изображение для метки в конце
+    end_marker = f"{end[0]},{end[1]},pm2ntl3"
+
+    # Параметры линии (красная пунктирная линия толщиной 4)
+    line = f"pl=c:ff0000ff,w:4,{get_coordinates_string(coordinates)}"
+
+    center = calculate_center(coordinates)
+
+    # Формирование полного URL для Static Maps API
+    static_map_url = (
+        f"https://static-maps.yandex.ru/v1?"
+        f"l=map&"
+        f"size=500,300&"
+        f"ll={center[0]},{center[1]}&"
+        # f"pt={start_marker}~{end_marker}~"
+        f"{line}&"
+        # f"size=500,300,lang=ru_RU"
+        f"pt={start_marker}~{end_marker}&"
+        f"apikey=3c6f0569-0691-4254-8518-55a4f66b4295"
+    )
+    # static_map_url = json.loads(route.coordinates)[0]
+
+    # Результирующий URL
+    return static_map_url
+
+def get_coordinates_string(coordinates):
+    return ",".join([f"{lon},{lat}" for lon, lat in coordinates])
+
+def calculate_center(coordinates):
+    avg_lat = sum(lat for lon, lat in coordinates) / len(coordinates)
+    avg_lon = sum(lon for lon, lat in coordinates) / len(coordinates)
+    return avg_lon, avg_lat
